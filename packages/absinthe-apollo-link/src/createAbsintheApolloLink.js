1538a679949608eaca1154399b633de94cb5ed97
Dirk Elmendorf <dirk@r26d.com>
2023-03-25 10:17:02 -0500

    infra: Changing compose libraies ramda ->rambda to fix issue with current version of node.
    Signed-off-by: Dirk Elmendorf <dirk@r26d.com>

--- packages/absinthe-apollo-link/src/createAbsintheApolloLink.js
+++ packages/absinthe-apollo-link/src/createAbsintheApolloLink.js
@@ -7,7 +7,9 @@ import {
 //import compose from 'ramda/src/compose'
 //12/7/22 - this is not compatible with node 18
 //https://github.com/ramda/ramda/commit/d0099847b50660f3ddefc528c0813e9983d6b394
-import { compose } from 'ramda'
+// import { compose } from 'ramda'
+//as of 3/25/23 - no updates to ramda - trying a different lib
+import {compose } from 'rambda'
 import { print } from 'graphql'
 
 const unobserveOrCancelIfNeeded = (absintheSocket, notifier, observer) => {
